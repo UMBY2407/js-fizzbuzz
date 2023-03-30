b400cc0bfbe3b63a09648d24b212491acdb5c1a2
UMBY2407 <umby2407@gmail.com>
2023-03-30 15:13:00 +0200

    Added boxes in html and modified style.css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 for(let i=1; i<=100;i++){
     if(i%3==0){
         if(i%5==0){
-            console.log("fizzbuzz");
+            //console.log("fizzbuzz");
+            document.getElementById("boxContainer").innerHTML += `
+            <div class="square">
+                <p>fizzbuzz</p>
+            </div>
+            `;
         }else{
-            console.log("fizz");
+            //console.log("fizz");
+            document.getElementById("boxContainer").innerHTML += `
+            <div class="square">
+                <p>fizz</p>
+            </div>
+            `;
         }
     }else if(i%5==0){
-        console.log("buzz");
+        //console.log("buzz");
+        document.getElementById("boxContainer").innerHTML += `
+        <div class="square">
+            <p>buzz</p>
+        </div>
+        `;
     }else{
-        console.log(i);
+        //console.log(i);
+        document.getElementById("boxContainer").innerHTML += `
+        <div class="square">
+            <p>${i}</p>
+        </div>
+        `;
     }
 }
